a29030c1af3dbc454664c067c29283b5ff64e3a2
LinuzTorvalds <grumec951@gmail.com>
2022-07-03 11:27:10 -0300

    some update from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import app from './app'
-import * as dotenv from 'dotenv'
 import { env } from 'node:process'
 
-dotenv.config()
+const PORT: number = env.PORT || 3001
 
-app.listen(env.PORT || 3001, () =>
-  console.log(`Pai ta on familia o_O on PORT ${env.PORT}`)
-)
+app.listen(PORT, () => console.log(`Pai ta on familia o_O on PORT ${PORT}`))
